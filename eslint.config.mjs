ad100b6ee78334dda4d149706a3ec604f913df07
Wi hyun Jeong <wihyunjeong@Wiui-noteubug.local>
2025-11-28 14:52:03 +0900

    프로젝트 최초 설치 작업중

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,16 +16,15 @@ const eslintConfig = defineConfig([
   {
     // 커스텀 규칙 추가
     rules: {
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
       "@typescript-eslint/no-explicit-any": "off",
       "jsx-quotes": ["error", "prefer-double"],
       semi: ["error", "always"],
       "@typescript-eslint/no-empty-object-type": "off",
       "react/jsx-max-props-per-line": ["error", { maximum: 1 }],
       "react-hooks/exhaustive-deps": "off",
+			"no-unused-vars": "warn",
+			"@typescript-eslint/no-unused-vars": "warn",
+			//"no-console": "warn",
     },
   },
 ]);
