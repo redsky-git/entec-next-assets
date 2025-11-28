49ec49a5817b25b75b031594eb79d32f1cbfbf0d
Wi hyun Jeong <wihyunjeong@Wiui-noteubug.local>
2025-11-28 15:45:30 +0900

    src 폴더 생성 후 구조 다시 셋팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,9 +22,10 @@ const eslintConfig = defineConfig([
       "@typescript-eslint/no-empty-object-type": "off",
       "react/jsx-max-props-per-line": ["error", { maximum: 1 }],
       "react-hooks/exhaustive-deps": "off",
-			"no-unused-vars": "warn",
-			"@typescript-eslint/no-unused-vars": "warn",
-			//"no-console": "warn",
+      // "no-unused-vars": "warn", 옵션은 사용되지 않는 변수에 대해 eslint가 경고를 표시하도록 설정합니다.
+      // TypeScript를 사용할 때는 "@typescript-eslint/no-unused-vars"를 대신 사용하는 것이 더 좋습니다.
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
 ]);
