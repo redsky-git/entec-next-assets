e440d8e26033c0f1d85d087687aab4c007667376
Wi hyun Jeong <wihyunjeong@Wiui-noteubug.local>
2025-11-28 16:43:15 +0900

    shadcn/ui 설치 및 관련 파일 url 변경 및 세팅 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,22 @@ export default function Home() {
 		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
 				<Image
+					src="/vercel.svg"
+					alt="Vercel logo"
+					width={100}
+					height={100}
+					style={{
+						filter:
+							'brightness(0) saturate(100%) invert(27%) sepia(98%) saturate(7426%) hue-rotate(356deg) brightness(91%) contrast(135%)',
+					}}
+				/>
+				<Image
 					className="dark:invert"
 					src="/next.svg"
 					alt="Next.js logo"
 					width={100}
 					height={20}
+					style={{ color: 'red' }}
 					priority
 				/>
 				<div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
