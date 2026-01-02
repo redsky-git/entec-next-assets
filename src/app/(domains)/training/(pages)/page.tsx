6c90e9fd22dd2bbacd737b64aa95e72858b639c6
Wi hyun Jeong <wihyunjeong@Wiui-noteubug.local>
2026-01-02 16:36:17 +0900

    페이지 만들기 실습 페이지 작성

---
+++ src/app/(domains)/training/(pages)/page.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import type { IComponent } from '@/core/types/common';
+import { Separator } from '@/core/components/shadcn/ui/separator';
+import RunCodeblock from '@/app/(domains)/training/_components/RunCodeblock';
+
+interface ITrainingMainProps {
+	test?: string;
+}
+
+const TrainingMain: IComponent<ITrainingMainProps> = () => {
+	return (
+		<>
+			<div className="flex min-w-0 flex-1 flex-col">
+				<div className="h-(--top-spacing) shrink-0" />
+				<div className="mx-auto flex w-full  min-w-0 flex-1 flex-col gap-8 px-4 py-6 text-neutral-800 md:px-0 lg:py-8 dark:text-neutral-300">
+					<div className="flex flex-col gap-2">
+						<div className="flex items-start justify-between">
+							<h1 className="scroll-m-20 text-4xl font-semibold tracking-tight sm:text-3xl xl:text-4xl">
+								페이지 및 레이아웃 만들기
+							</h1>
+							<div className="docs-nav bg-background/80 border-border/50 fixed inset-x-0 bottom-0 isolate z-50 flex items-center gap-2 border-t px-6 py-4 backdrop-blur-sm sm:static sm:z-0 sm:border-t-0 sm:bg-transparent sm:px-0 sm:pt-1.5 sm:backdrop-blur-none">
+								&nbsp;
+							</div>
+						</div>
+						<p className="text-muted-foreground text-[1.05rem] text-balance sm:text-base">
+							페이지 만들기 첫 실습입니다.
+						</p>
+					</div>
+					<div className="w-full flex-1 *:data-[slot=alert]:first:mt-0">
+						<Separator className="my-6" />
+						{/* example 블럭요서 START */}
+						<div className="flex flex-col gap-2 pt-6">
+							<div className="flex items-start justify-between">
+								<h2
+									data-shorcut="true"
+									className="scroll-m-20 text-3xl font-semibold tracking-tight sm:text-3xl xl:text-3xl mb-2"
+								>
+									페이지 기본 템플릿
+								</h2>
+							</div>
+							<p className="text-muted-foreground text-[1.05rem] text-balance sm:text-base">
+								모든 페이지(<strong>page.tsx</strong>)는 다음과 같은 기본 템플릿을 복사하여 생성합니다.
+							</p>
+							<div className="flex justify-center py-1">
+								<RunCodeblock
+									lineNumbers={false}
+									showCodeBlockCopyButton={false}
+									showCollapsed={false}
+									rounded={false}
+									codeTemplate={`// ========================================================
+// 페이지 만들기 기본 템플릿
+// ========================================================
+ 
+// 페이지 컴포넌트의 기본 타입 가져오기
+import type { IComponent } from '@/core/types/common';
+ 
+// 페이지 컴포넌트의 Props 타입 정의
+interface IPageTemplateProps {
+	// test?: string;
+}
+ 
+// 페이지 컴포넌트 본체
+const PageTemplate: IComponent<IPageTemplateProps> = () => {
+	return (<div>기본 템플릿 페이지입니다.</div>);
+};
+// 페이지 컴포넌트 이름 정의
+PageTemplate.displayName = 'PageTemplate';
+export default PageTemplate;`}
+								/>
+							</div>
+						</div>
+						{/* example 블럭요서 END */}
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+// 페이지 컴포넌트 이름 정의
+TrainingMain.displayName = 'TrainingMain';
+export default TrainingMain;
---
+++ src/app/(domains)/training/_components/RunCodeblock.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import type { IComponent } from '@/core/types/common';
+import type { JSX } from 'react';
+
+import UICodeBlockEx from '@/shared/components/common/ui/UICodeBlockEx';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@components/shadcn/ui/tabs';
+
+interface IRunCodeblockProps {
+	children?: any;
+	codeTemplate?: string;
+	title?: string;
+	lineNumbers?: boolean;
+	showCollapsed?: boolean;
+	showCodeBlockCopyButton?: boolean;
+	rounded?: boolean;
+}
+
+const RunCodeblock: IComponent<IRunCodeblockProps> = ({
+	children,
+	codeTemplate = '',
+	title = '',
+	lineNumbers = true,
+	showCodeBlockCopyButton = true,
+	showCollapsed = true,
+	rounded = true,
+}): JSX.Element => {
+	return (
+		<Tabs defaultValue="preview">
+			<TabsList
+				className="bg-transparent p-0 h-auto rounded-none shadow-none"
+				style={{ height: title.length === 0 ? '0' : `${title.length * 0.03}rem` }}
+			>
+				<TabsTrigger
+					value="preview"
+					className="text-lg font-semibold text-foreground data-[state=active]:bg-transparent data-[state=active]:shadow-none rounded-none pb-2 px-1 shadow-none"
+				>
+					{title}
+				</TabsTrigger>
+			</TabsList>
+			<TabsContent value="preview">
+				<div className="grid">
+					<div className="w-full min-w-0">{children}</div>
+					<div className="w-full min-w-0 overflow-x-auto">
+						<UICodeBlockEx
+							showHeader={false}
+							language="tsx"
+							filename="SamplePage.tsx"
+							className={rounded ? 'rounded-t-none rounded-b-md' : 'rounded-none'}
+							lineNumbers={lineNumbers}
+							showCodeBlockCopyButton={showCodeBlockCopyButton}
+							showCollapsed={showCollapsed}
+						>
+							{codeTemplate || ''}
+						</UICodeBlockEx>
+					</div>
+				</div>
+			</TabsContent>
+		</Tabs>
+	);
+};
+
+RunCodeblock.displayName = 'RunCodeblock';
+export default RunCodeblock;
